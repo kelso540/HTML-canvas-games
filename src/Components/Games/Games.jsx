e8b7c2eb462a4b53c5e0abc368e957e4fec36155
Kelsey Jackson <kels@Kelseys-Laptop.local>
2023-09-12 20:55:59 -0400

    changed div to a tag

--- src/Components/Games/Games.jsx
+++ src/Components/Games/Games.jsx
@@ -5,25 +5,25 @@ function Games({ scroll }) {
   return (
     <div className='HTMLGamesDiv'>
     <h3>BREAKOUT</h3>
-    <div className='gameCard breakout' href='https://quiet-malasada-97bcee.netlify.app/'style={{
+    <a className='gameCard breakout' href='https://quiet-malasada-97bcee.netlify.app/'style={{
     "--deg": `${ scroll * 6 }deg`
-  }}></div>
+  }}></a>
     <h3>ROBOT RUN</h3>
-    <div className='gameCard robotRun' href='https://thunderous-malasada-fb15ce.netlify.app/'style={{
+    <a className='gameCard robotRun' href='https://thunderous-malasada-fb15ce.netlify.app/'style={{
     "--deg": `${ scroll * 4 }deg`
-  }}></div>
+  }}></a>
     <h3>SNAKE</h3>
-    <div className='gameCard snake' href='https://ephemeral-rugelach-1cf859.netlify.app/'style={{
+    <a className='gameCard snake' href='https://ephemeral-rugelach-1cf859.netlify.app/'style={{
     "--deg": `${ scroll * 2 }deg`
-  }}></div>
+  }}></a>
     <h3>SPACE GAME</h3>
-    <div className='gameCard spaceGame' href='https://gleaming-cendol-896cb6.netlify.app/'style={{
+    <a className='gameCard spaceGame' href='https://gleaming-cendol-896cb6.netlify.app/'style={{
     "--deg": `${ scroll * 5 }deg`
-  }}></div>
+  }}></a>
     <h3>HONEY POT</h3>
-    <div className='gameCard beeGame' href='https://dulcet-souffle-26ebb6.netlify.app/'style={{
+    <a className='gameCard beeGame' href='https://dulcet-souffle-26ebb6.netlify.app/'style={{
     "--deg": `${ scroll * 3 }deg`
-  }}></div>
+  }}></a>
   </div>
   )
 }
